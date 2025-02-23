607fcac18a00a53903d3f0dd5f1902350ab4023f
danferdiansyah <danferdianstyle@gmail.com>
2025-02-23 13:53:02 +0700

    feat: added templates for create, edit, and car list. fixed the car service implementation

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+@Service
 public class CarServiceImpl implements CarService {
     @Autowired
     private CarRepository carRepository;
