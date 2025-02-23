02d0824521a561cd867491d98cf8979cf05fa63b
danferdiansyah <danferdianstyle@gmail.com>
2025-02-23 12:13:54 +0700

    ci: configuring sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,25 @@
 plugins {
     java
     jacoco
-    id("java")
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "danferdiansyah_eshop")
+        property("sonar.projectName", "eshop")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
-sonar {
-    properties {
-        property("sonar.projectKey", "danferdiansyah_eshop")
-        property("sonar.organization", "danferdiansyah")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -37,34 +35,31 @@ repositories {
 
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
+val webdriverManagerVersion = "5.6.3"
 val junitJupiterVersion = "5.9.1"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
-    compileOnly("org.projectlombok:lombok")
-
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdriverManagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
-}
 
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
 
 tasks.register<Test>("unitTest") {
     description = "Runs unit tests."
     group = "verification"
 
     filter {
-        excludeTestsMatching("*FunctionalTest")
+        excludeTestsMatching("*FunctionalTest*")
     }
 }
 
@@ -73,7 +68,7 @@ tasks.register<Test>("functionalTest") {
     group = "verification"
 
     filter {
-        includeTestsMatching("*FunctionalTest")
+        includeTestsMatching("*FunctionalTest*")
     }
 }
 
@@ -83,7 +78,7 @@ tasks.withType<Test>().configureEach {
 
 tasks.test {
     filter {
-        excludeTestsMatching("*FunctionalTest")
+        excludeTestsMatching("*FunctionalTest*")
     }
 
     finalizedBy(tasks.jacocoTestReport)
@@ -91,4 +86,8 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+    reports {
+        html.required = true
+        xml.required = true
+    }
 }
